86526de790e4f58204fa1bce7087013c03180ff3
sbirdsill <43529733+sbirdsill@users.noreply.github.com>
2019-07-14 22:44:06 -0700

    Update Get-Inactive-AD-Users.ps1

--- Get-Inactive-AD-Users.ps1
+++ Get-Inactive-AD-Users.ps1
@@ -3,36 +3,36 @@
 # Verifies that the script is running as admin
 function Check-IsElevated
 {
-    $id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-    $p = New-Object System.Security.Principal.WindowsPrincipal($id)
+  $id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+  $p = New-Object System.Security.Principal.WindowsPrincipal ($id)
 
-    if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))
-    {
-        Write-Output $true
-    }            
-    else
-    {
-        Write-Output $false
-    }       
+  if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))
+  {
+    Write-Output $true
+  }
+  else
+  {
+    Write-Output $false
+  }
 }
 
 if (!(Check-IsElevated))
 {
-    throw "Please run this script from an elevated PowerShell prompt"            
+  throw "Please run this script from an elevated PowerShell prompt"
 }
 
-import-module activedirectory 
+Import-Module activedirectory
 $DaysInactive = Read-Host "Enter the max age in days [30]"
 if ($DaysInactive -eq "") { $DaysInactive = 30 }
-$time = (Get-Date).Adddays(-($DaysInactive))
+$time = (Get-Date).AddDays(- ($DaysInactive))
 
 Write-Host ""
 Write-Host "List of users that have been inactive for at least $DaysInactive days:"
- 
+
 # Show users that have not logged in $DaysInactive
-Get-ADUser -Filter {LastLogonTimeStamp -lt $time -and enabled -eq $true} -Properties LastLogonTimeStamp |
-select-object Name,@{Name="Stamp"; Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}},DistinguishedName |
+Get-ADUser -Filter { LastLogonTimeStamp -LT $time -and enabled -EQ $true } -Properties LastLogonTimeStamp |
+Select-Object Name,@{ Name = "Stamp"; Expression = { [datetime]::FromFileTime($_.lastLogonTimestamp) } },DistinguishedName |
 Sort -Property Stamp
 
 # Show users that have never logged in (the Stamp will show "{}")
-Get-ADUser -Filter {(lastlogontimestamp -notlike "*") -and (enabled -eq $true) -and (whencreated -lt $time)}
+Get-ADUser -Filter { (LastLogonTimeStamp -NotLike "*") -and (enabled -EQ $true) -and (whencreated -LT $time) }
